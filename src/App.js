9f5a8456ea7a2bc568310c0421015fa0a4292dbb
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-04-06 17:58:26 -0400

    Imported React Router and set up Route paths/components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense component 
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from the help page
+  </div>
+)
 
 // Blank slate as we are moving on to a different project (preserved previous one inside the practice folder)
 
@@ -6,6 +31,14 @@ export default function App() {
   return (
     <>
      Expensify Boilerplate
+
+     {/* Exact avoids accidental loading of one app on top of another */}
+     <Route exact path="/" component={ExpenseDashboardPage} />
+     <Route path="/add" component={AddExpensePage} />
+     <Route path="/edit" component={EditExpensePage} />
+     <Route path="/help" component={HelpPage} />
     </>
+
   )
+
 }
