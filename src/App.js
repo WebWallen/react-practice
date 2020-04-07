a1f470b22c96e5a778d8e23469682bd1d880116b
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-04-07 18:29:47 -0400

    Added a Switch, Page Not Found Component for Nonexistent URL, and NavLinks to prevent page re-load upon updated Routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import './App.css'
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
 
 const ExpenseDashboardPage = () => (
   <div>
@@ -25,18 +26,40 @@ const HelpPage = () => (
   </div>
 )
 
+const NotFoundPage = () => (
+  <div>
+    {/* Links eliminate full page refresh, merely swaps in new content */}
+    404 - <Link to="/">Go Home</Link>
+  </div>
+)
+
+// Header isn't in Switch because we want it to appear in every component
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    {/* Navlink is identical to Link but better as we can add style props -- add exact so it only affects one route at a time */}
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/add" activeClassName="is-active">Add Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+)
+
 // Blank slate as we are moving on to a different project (preserved previous one inside the practice folder)
 
 export default function App() {
   return (
     <>
-     Expensify Boilerplate
-
+     <Header />
+     <Switch>
      {/* Exact avoids accidental loading of one app on top of another */}
-     <Route exact path="/" component={ExpenseDashboardPage} />
-     <Route path="/add" component={AddExpensePage} />
-     <Route path="/edit" component={EditExpensePage} />
-     <Route path="/help" component={HelpPage} />
+      <Route exact path="/" component={ExpenseDashboardPage} />
+      <Route path="/add" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      {/* Switch allows us to add Not Found Page if URL doesn't match a path */}
+      <Route component={NotFoundPage} />
+     </Switch>
     </>
 
   )
