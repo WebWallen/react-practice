efcae9583124852f453d50c55d4611c0a75a466c
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-04-10 19:00:00 -0400

    Slight touch-up

--- src/practice/redux-101.js
+++ src/practice/redux-101.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-// Called once to create store, which tracks changing data over time
+// Called once to create store, which tracks changing data over time in a global container
 
 // Set up state with default value
 const store = createStore((state = { count : 0 }) => {
