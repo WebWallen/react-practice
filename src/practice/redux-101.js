3e1d7279f15a8528ebd0f34eef041b39e87f3cb5
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-04-20 20:24:26 -0400

    Refactored to include action creators and passed solo practice exercise.

--- src/practice/redux-101.js
+++ src/practice/redux-101.js
@@ -2,25 +2,46 @@ import React from 'react';
 import { createStore } from 'redux';
 // Called once to create store, which tracks changing data over time in a global container
 
+// Action Generators used to handle all action types
+// Also makes for easier debugging due to specific errors
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    // Passing in default to prevent incorrect data type
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count = 1 } = {}) => ({
+    type: 'SET',
+    count
+});
+
+// Doesn't need instructions, parameters, or default value since it always resets to 0 as already described in store
+const resetCount = () => ({ 
+    type: 'RESET'
+})
+
 export default function ReduxPractice() {
     // Set up state with default value
     const store = createStore((state = { count : 0 }, action) => {
         switch (action.type) {
             case 'INCREMENT':
                 // If a number is attached, add by that; if not, increment by 1
-                const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
                 return {
-                    count: state.count + incrementBy
+                    // Must add "action." due to refactoring with action creators (also allowed us to remove consts with functional logic)
+                    count: state.count + action.incrementBy
                 } 
             case 'DECREMENT':
-                const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
                 return {
-                    count: state.count - decrementBy
+                    count: state.count - action.decrementBy
                 }
             case 'SET': 
             // Dynamically set a new count
                 return {
-                    
                     count: action.count
                 }
             case 'RESET': 
@@ -39,29 +60,19 @@ export default function ReduxPractice() {
     // To unsubscribe, assign to variable and call again
 
     // Actions - object that gets sent to store describing its type
-    store.dispatch({
-        // Convention = caps and underscores separating words
-        type: 'INCREMENT',
-        // Can now pass in dynamic values with this variable
-        incrementBy: 5
-    });
-
-    // unsubscribe()
-
-    store.dispatch({
-        type: 'RESET'
-    })
-
-    store.dispatch({
-        // Convention = caps and underscores separating words
-        type: 'DECREMENT',
-        decrementBy: 5
-    });
-
-    store.dispatch({
-        type: 'SET',
-        count: 101
-    })
+    store.dispatch(incrementCount({ incrementBy: 5 }));
+    store.dispatch(incrementCount());
+    store.dispatch(decrementCount());
+    store.dispatch(decrementCount({ decrementBy: 5}));
+    store.dispatch(setCount({ count: 10}));
+    store.dispatch(resetCount());
+    
+    // store.dispatch({
+    //     // Convention = caps and underscores separating words
+    //     type: 'INCREMENT',
+    //     // Can now pass in dynamic values with this variable
+    //     incrementBy: 5
+    // });
 
     return (
         <div>Nothing to see here.</div>
