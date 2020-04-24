08a24413a150b744011496f9be221ca0a21c9462
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-04-23 20:17:23 -0400

    Edit Expense Action Generator and Beginnings of Filter Expenses Reducer

--- src/practice/redux-expensify.js
+++ src/practice/redux-expensify.js
@@ -26,7 +26,13 @@ export default function ReduxExpensify() {
     const removeExpense = ({ id } = {}) => ({
         type: 'REMOVE_EXPENSE',
         id
-    })
+    });
+
+    const editExpense = (id, updates) => ({
+        type: 'EDIT_EXPENSE',
+        id,
+        updates
+    });
 
     // Expenses reducer
     const expensesReducerDefaultState = [];
@@ -34,18 +40,36 @@ export default function ReduxExpensify() {
         switch (action.type) {
             case 'ADD_EXPENSE':
                 // Don't want to manipulate array, make new one with newest expense
-                // return state.concat(action.expense)
                 return [
                     ...state,
                     action.expense
                 ]
+
             case 'REMOVE_EXPENSE':
-                // Filter doesn't change array, returns fresh one with specified filtering (id not matching the action's id)
+                // Filter doesn't change array, returns fresh one with specified filtering (id's not matching the removed action's id)
                 return state.filter(({ id }) => id !== action.id);
+
+            case 'EDIT_EXPENSE':
+                // Map is a good choice because it allows us to modify an array (container for expenses) and return without manipulating original
+                return state.map((expense) => {
+                    if (expense.id === action.id) {
+                        return {
+                            ...expense,
+                            ...action.updates
+                        }
+                    } else return expense; 
+                })
+
             default: 
                 return state;
         }
     };
+
+    // Set Filter Action Generator
+    const setTextFilter = (text = '') => ({
+        type: 'SET_TEXT_FILTER',
+        text
+    })
 
     // Filters reducer 
     const filtersReducerDefaultState = {
@@ -57,6 +81,14 @@ export default function ReduxExpensify() {
 
     const filtersReducer = (state = filtersReducerDefaultState, action) => {
         switch (action.type) {
+            case 'SET_TEXT_FILTER': 
+                return {
+                    // Pass in current state, then change text property to action's text value
+                    // Note: can't use filter because we are returning an object vs an array here
+                    ...state,
+                    text: action.text
+                }
+
             default: 
                 return state;
         }
@@ -77,7 +109,11 @@ export default function ReduxExpensify() {
     const expenseOne = store.dispatch(addExpense({ description: 'Coffee', amount: 2 }));
     const expenseTwo = store.dispatch(addExpense({ description: 'DC Universe', amount: 7 }));
 
-    store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+    store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+    store.dispatch(editExpense(expenseTwo.expense.id, { amount: 10 } ));
+
+    store.dispatch(setTextFilter('rent'));
+    store.dispatch(setTextFilter());
 
     const demoState = {
         expenses: [{
@@ -100,13 +136,14 @@ export default function ReduxExpensify() {
     )
 }
 
-// Add expense
+// Object spread practice
+const user = {
+    name: 'Jen',
+    age: 24
+};
 
-// Remove expense
-// Edit expense
-// Set text filter
-// Sort by date
-// Sort by amount
-// Set start date
-// Set end date
-// ^ too many actions for one reducer, splitting it up
+console.log({
+    ...user,
+    location: 'Philadelphia',
+    age: 27
+})
