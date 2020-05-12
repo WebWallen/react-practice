65be2c9e385729fb328585fee024051a3088290e
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-05-12 18:38:42 -0400

    Imported Moment/React Dates. Made form to add expenses and calendar where user can select the date (overwrote default to allow selection of past dates, which would be an expected feature).

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: '',
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
+    }
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        // Digit may have decimal but only 1, must come at the end ($), and limit of 2 numbers after it
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }))
+        } // No need for else/error because the Regex makes it impossible to submit info with incorrect format
+    }
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    }
+
+    onFocusChange = (focused) => {
+        this.setState(() => ({ calendarFocused: focused }));
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        // Overrides default so we can pick past dates
+                        isOutsideRange={() => false}
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        )
+    }
+}
