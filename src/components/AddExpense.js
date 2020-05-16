e3c5331279c87d4e8999f2c8beeb427219119ef7
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-05-15 20:40:16 -0400

    Wired up add expense so the form inputs render in the user's dashboard. Required some debugging but this was successful!

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpense = () => (
+const AddExpense = (props) => (
     <div>
       <h1>Add Expense</h1>
-      <ExpenseForm />
+      <ExpenseForm 
+      // Wire button up by dispatching the right action with user data and redirecting to dashboard
+        onSubmit={(expense) => {
+          console.log(expense);
+          props.dispatch(addExpense(expense));
+          props.history.push('/dashboard');
+        }}  
+      />
     </div>
 );
 
-export default AddExpense;
+export default connect()(AddExpense);
